8764b5752ec740ffbff20cfc7f98e61d1da67a18
jethrinfox <jethrinfox@gmail.com>
2021-01-11 21:14:20 -0300

    added production env & input type as any

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -8,22 +8,25 @@ import {
 import { useField } from "formik";
 import React, { InputHTMLAttributes } from "react";
 
-type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
-	name: string;
+type InputFieldProps = InputHTMLAttributes<
+	HTMLInputElement | HTMLTextAreaElement
+> & {
 	label: string;
+	name: string;
 	textarea?: boolean;
 };
 
 export const InputField: React.FC<InputFieldProps> = ({
 	label,
-	size: _,
 	textarea,
+	size: _,
 	...props
 }) => {
-	let InputOrTextarea = textarea ? Textarea : Input;
-
+	let InputOrTextarea = Input as any;
+	if (textarea) {
+		InputOrTextarea = Textarea;
+	}
 	const [field, { error }] = useField(props);
-
 	return (
 		<FormControl isInvalid={!!error}>
 			<FormLabel htmlFor={field.name}>{label}</FormLabel>
